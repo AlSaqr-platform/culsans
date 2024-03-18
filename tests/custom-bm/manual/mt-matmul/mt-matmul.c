2df2191a61413f94b0310536bede3f35d32944f2
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2024-03-18 15:01:03 +0100

    Adapt mt-matmul from riscv-tests

---
+++ tests/custom-bm/manual/mt-matmul/mt-matmul.c
@@ -0,0 +1,58 @@
+// See LICENSE for license details.
+
+//**************************************************************************
+// Multi-threaded Matrix Multiply benchmark
+//--------------------------------------------------------------------------
+// TA     : Christopher Celio
+// Student:
+//
+//
+// This benchmark multiplies two 2-D arrays together and writes the results to
+// a third vector. The input data (and reference data) should be generated
+// using the matmul_gendata.pl perl script and dumped to a file named
+// dataset.h.
+
+//--------------------------------------------------------------------------
+// Includes
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+//--------------------------------------------------------------------------
+// Input/Reference Data
+
+#include "dataset.h"
+
+
+//--------------------------------------------------------------------------
+// Basic Utilities and Multi-thread Support
+
+#include "util.h"
+#include "test_utils.h"
+
+
+//--------------------------------------------------------------------------
+// matmul function
+
+extern void matmul(const size_t coreid, const size_t ncores, const size_t lda,  const data_t A[], const data_t B[], data_t C[] );
+
+
+//--------------------------------------------------------------------------
+// Main
+//
+// all threads start executing thread_entry(). Use their "coreid" to
+// differentiate between threads (each thread is running on a separate core).
+
+void thread_entry(int cid, int nc)
+{
+   static data_t results_data[ARRAY_SIZE] __attribute__((section(".cache_share_region")));
+
+   stats(matmul(cid, nc, DIM_SIZE, input1_data, input2_data, results_data); barrier_wait(nc), DIM_SIZE/DIM_SIZE/DIM_SIZE);
+
+   int res = verify(ARRAY_SIZE, results_data, verify_data);
+
+   exit(res);
+}
